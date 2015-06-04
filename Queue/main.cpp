d632b00f851a0a7eb290a48aed8ecb3a8f13a1e1
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-06-04 12:13:47 +0200

    begin

---
+++ Queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by Gabriele Carrettoni on 04/06/15.
+//  Copyright (c) 2015 Gabriele Carrettoni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
