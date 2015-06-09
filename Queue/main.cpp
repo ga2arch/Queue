822406a0f06dba3e6adce2d15cadf2fa7f372a95
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-06-09 15:31:41 +0200

    added test push/pop

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -8,17 +8,62 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <assert.h>
 #include "Queue.h"
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+void test_push_pop_primitive() {
+    cout << "PUSH/POP: ";
+    
+    Queue<int> q1;
+    queue<int> q2;
+    
+    q1.push(1);
+    q2.push(1);
+    
+    q1.push(2);
+    q2.push(2);
+    
+    q1.push(3);
+    q2.push(3);
+    
+    while (q1.size() > 0) {
+        assert(q1.front() == q2.front());
+        q1.pop();
+        q2.pop();
+    }
+    
+    cout << "PASSED" << endl;
+}
+
+void test_push_pop_complex() {
+    cout << "PUSH/POP: ";
     
     Queue<string> q1;
+    queue<string> q2;
+    
     q1.push("ciao");
+    q2.push("ciao");
+    
     q1.push("come");
+    q2.push("come");
     
-    transformif(q1, [](string x){ return x == "ciao"; }, [](string x){ return "bella"; });
+    q1.push("va");
+    q2.push("va");
     
-    cout << q1;
+    while (q1.size() > 0) {
+        assert(q1.front() == q2.front());
+        q1.pop();
+        q2.pop();
+    }
+    
+    cout << "PASSED" << endl;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    test_push_pop_primitive();
+    
 }
